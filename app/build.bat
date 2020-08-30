79b77505f05d996769726ed780640485e72b0ce2
Kevin <chenkevinh11@gmail.com>
2020-08-29 22:13:08 -0700

    Updated build script to use user config directory

--- app/build.bat
+++ app/build.bat
@@ -1,2 +1,2 @@
-west build -d build\left -- -DSHIELD=lily58_left
-west build -d build\right -- -DSHIELD=lily58_right
+west build -d build\left -- -DSHIELD=lily58_left -DZMK_CONFIG="C:/Users/Kevin/zmk-config/config"
+west build -d build\right -- -DSHIELD=lily58_right -DZMK_CONFIG="C:/Users/Kevin/zmk-config/config"
