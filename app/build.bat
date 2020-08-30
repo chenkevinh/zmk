77d365e9d528478690649842358973dea3621562
Kevin <chenkevinh11@gmail.com>
2020-08-29 18:51:54 -0700

    Updated build commands

---
+++ app/build.bat
@@ -0,0 +1,2 @@
+west build -d build\left -- -DSHIELD=lily58_left
+west build -d build\right -- -DSHIELD=lily58_right
--- app/flash.bat
+++ app/flash.bat
@@ -1,2 +1,2 @@
 timeout 10 
-copy "C:\Users\Kevin\zmk\app\build\zephyr\zmk.uf2" D:
+copy "C:\Users\Kevin\zmk\app\build\left\zephyr\zmk.uf2" D:
